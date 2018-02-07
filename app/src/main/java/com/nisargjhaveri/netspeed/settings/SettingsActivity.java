43bddbd1618dda64237d233c0979253b676fe387
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2018-02-07 20:55:49 +0530

    Move logic from SettingsActivity to SettingsFragment

--- app/src/main/java/com/nisargjhaveri/netspeed/settings/SettingsActivity.java
+++ app/src/main/java/com/nisargjhaveri/netspeed/settings/SettingsActivity.java
@@ -1,63 +1,16 @@
 package com.nisargjhaveri.netspeed.settings;
 
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
-import com.nisargjhaveri.netspeed.IndicatorServiceHelper;
 import com.nisargjhaveri.netspeed.R;
 
 public final class SettingsActivity extends AppCompatActivity {
-
-    private SharedPreferences mSharedPref;
-
-    private final OnSharedPreferenceChangeListener mSettingsListener = new OnSharedPreferenceChangeListener() {
-        @Override
-        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-            if (key.equals(Settings.KEY_INDICATOR_ENABLED)) {
-                if (mSharedPref.getBoolean(Settings.KEY_INDICATOR_ENABLED, true)) {
-                    startIndicatorService();
-                } else {
-                    stopIndicatorService();
-                }
-            } else if (!key.equals(Settings.KEY_START_ON_BOOT)
-                    && !key.equals(Settings.KEY_INDICATOR_STARTED)) {
-                startIndicatorService();
-            }
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
-        mSharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-
-        if (mSharedPref.getBoolean(Settings.KEY_INDICATOR_ENABLED, true)) {
-            startIndicatorService();
-        }
     }
 
-    @Override
-    protected void onResume() {
-       super.onResume();
-        mSharedPref.registerOnSharedPreferenceChangeListener(mSettingsListener);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mSharedPref.unregisterOnSharedPreferenceChangeListener(mSettingsListener);
-    }
-
-    private void startIndicatorService() {
-        IndicatorServiceHelper.startService(this);
-    }
-
-    private void stopIndicatorService() {
-        IndicatorServiceHelper.stopService(this);
-    }
 }
--- app/src/main/java/com/nisargjhaveri/netspeed/settings/SettingsFragment.java
+++ app/src/main/java/com/nisargjhaveri/netspeed/settings/SettingsFragment.java
@@ -1,17 +1,67 @@
 package com.nisargjhaveri.netspeed.settings;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
+import com.nisargjhaveri.netspeed.IndicatorServiceHelper;
 import com.nisargjhaveri.netspeed.R;
 
 public final class SettingsFragment extends PreferenceFragment {
+    private SharedPreferences mSharedPref;
+    private Context mContext;
+
+    private final SharedPreferences.OnSharedPreferenceChangeListener mSettingsListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            if (key.equals(Settings.KEY_INDICATOR_ENABLED)) {
+                if (mSharedPref.getBoolean(Settings.KEY_INDICATOR_ENABLED, true)) {
+                    startIndicatorService();
+                } else {
+                    stopIndicatorService();
+                }
+            } else if (!key.equals(Settings.KEY_START_ON_BOOT)
+                    && !key.equals(Settings.KEY_INDICATOR_STARTED)) {
+                startIndicatorService();
+            }
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mContext = getActivity();
+
         addPreferencesFromResource(R.xml.preferences);
+
+        mSharedPref = PreferenceManager.getDefaultSharedPreferences(mContext);
+
+        if (mSharedPref.getBoolean(Settings.KEY_INDICATOR_ENABLED, true)) {
+            startIndicatorService();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mSharedPref.registerOnSharedPreferenceChangeListener(mSettingsListener);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mSharedPref.unregisterOnSharedPreferenceChangeListener(mSettingsListener);
+    }
+
+    private void startIndicatorService() {
+        IndicatorServiceHelper.startService(mContext);
+    }
+
+    private void stopIndicatorService() {
+        IndicatorServiceHelper.stopService(mContext);
     }
 
 }
